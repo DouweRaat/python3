b6e4d6443f6f7c0bd8b90518115b6a0143554e96
Douwe Raat <douwe.raat@coderclass.nl>
2019-09-03 14:23:13 +0200

    Toevoegen en verwijderen
    Je kan nu keys en values toevoegen en verwijderen.

--- Dictionaries opdracht 1.py
+++ Dictionaries opdracht 1.py
@@ -2,7 +2,23 @@ getallen = {"0": "10", "1": "20", "2": "30", "3": "40", "4": "50", "5": "60"}
 
 while True:
     getal = input()
-    if getal == "quit":
+    if getal == "add":
+        addkey = input("Key: ")
+        addvalue = input("Value: ")
+        getallen[key] = value
+    elif getal == "delete":
+        deletekey = input("Key: ")
+        if deletekey in getallen:
+            del getallen[deletekey]
+        else:
+            print("Deze key bestaat niet.")
+    elif getal == "all":
+        for key, value in getallen.items():
+            print(key, value)
+    elif getal == "quit":
         break
     else:
-        print(getallen[getal])
+        if getal in getallen:
+            print(getallen[getal])
+        else:
+            print("Deze key bestaat niet.")
